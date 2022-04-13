3997b0f17ec4123e4718ffc7ed7deae9b27ccc83
Victor Vattier <victor.vattier35@gmail.com>
2022-04-13 16:59:06 +0200

    global state logic done

--- wealthhealth/src/state/StateGlobale.jsx
+++ wealthhealth/src/state/StateGlobale.jsx
@@ -1,5 +1,5 @@
 import { useReducer } from "react"
-import employeeReducer from "./employeReducer"
+import employeeReducer, {ADD_EMPLOYEE} from "./employeReducer"
 import { EmployeeContext } from "./CreateContext"
 
 
@@ -7,17 +7,21 @@ function GlobalState() {
     const employee = []
     const [listState, dispatch] = useReducer(employeeReducer, {employeeList: []} )
 
-
-
-
+    function addEmployeeToTheList(employee) {
+        return dispatch({type: ADD_EMPLOYEE, employee: employee})
+    }
 
     return (
-        <EmployeeContext.Provider value={
-            employee : employee
-        }>
+        <EmployeeContext.Provider value={{
+            employee : employee,
+            list: listState,
+            addEmployeeToTheList: addEmployeeToTheList,
+        }}>
 
 
 
         </EmployeeContext.Provider>
     )
 }
+
+export default GlobalState
