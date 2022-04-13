52d57461570ea8de79209fa37157a67de36d1b63
Victor Vattier <victor.vattier35@gmail.com>
2022-04-13 14:50:30 +0200

    début css form

--- wealthhealth/src/components/EmployeeForm/EmployeeForm.jsx
+++ wealthhealth/src/components/EmployeeForm/EmployeeForm.jsx
@@ -1,8 +1,28 @@
+import '../../styles/Form.css'
 
 function EmployeeForm() {
     return (
         <section className="form-wrapper">
-            <h1>New Employee</h1>
+            <h1 className='form-title'>New Employee</h1>
+            <form action="#" id='create-employee-form' className="form" >
+                <fieldset >
+                    <legend>Employee</legend>
+                    <div>
+                        <label>First Name</label>
+                        <input type='text'></input>
+                    </div>
+                    <div>
+                        <label>Last Name</label>
+                        <input type='text'></input>
+                    </div>
+                    <div>
+                        <label>Date of Birth</label>
+                        <input type='date'></input>
+                    </div>
+                </fieldset>
+
+
+            </form>
         </section>
     )
 }
