9494f0acdf37c1df3b2f35691077c13619eb7c55
Victor Vattier <victor.vattier35@gmail.com>
2022-04-13 15:32:38 +0200

    fin du composant formulaire

---
+++ wealthhealth/src/components/DropdownMenu/DropdownMenu.jsx
@@ -0,0 +1,9 @@
+import Select, { createFilter } from 'react-select'
+
+function DropdownMenu() {
+    return (
+        <Select />
+    )
+}
+
+export default DropdownMenu
--- wealthhealth/src/components/EmployeeForm/EmployeeForm.jsx
+++ wealthhealth/src/components/EmployeeForm/EmployeeForm.jsx
@@ -1,23 +1,47 @@
 import '../../styles/Form.css'
+import DropdownMenu from '../DropdownMenu/DropdownMenu'
 
 function EmployeeForm() {
     return (
         <section className="form-wrapper">
             <h1 className='form-title'>New Employee</h1>
             <form action="#" id='create-employee-form' className="form" >
-                <fieldset >
+                <fieldset className='fieldset-name'>
                     <legend>Employee</legend>
-                    <div>
-                        <label>First Name</label>
+                    <div className='input-container'>
+                        <label className='input-label'>First Name</label>
                         <input type='text'></input>
                     </div>
-                    <div>
-                        <label>Last Name</label>
+                    <div className='input-container'>
+                        <label className='input-label'>Last Name</label>
                         <input type='text'></input>
                     </div>
-                    <div>
-                        <label>Date of Birth</label>
+                    <div className='input-container'>
+                        <label className='input-label'>Date of Birth</label>
                         <input type='date'></input>
+                    </div>
+                    <div className='input-container'>
+                        <label className='input-label'>Start Date</label>
+                        <input type='date'></input> 
+                    </div>
+                </fieldset>
+                <fieldset className='fieldset-adress'>
+                    <legend>Adress</legend>
+                    <div className='input-container'>
+                        <label className='input-label'>Street</label>
+                        <input type='text'></input>
+                    </div>
+                    <div className='input-container'>
+                        <label className='input-label'>City</label>
+                        <input type='text'></input>
+                    </div>
+                    <div className='input-container'>
+                        <label className='input-label'>State</label>
+                        <DropdownMenu />
+                    </div>
+                    <div className='input-container'>
+                        <label className='input-label'>ZipCode</label>
+                        <input type='text'></input>
                     </div>
                 </fieldset>
 
