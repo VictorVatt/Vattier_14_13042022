b733a0132b2eaa8a713a7ccd4cf6c081e2600bd0
Victor Vattier <victor.vattier35@gmail.com>
2022-04-16 19:19:37 +0200

    correction erreur redux

--- wealthhealth/src/utils/store.js
+++ wealthhealth/src/utils/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import newEmployeeReducer from "./reducers/newEmployeeReducer";
 
 const store = configureStore({
     reducer: {
         employees: newEmployeeReducer,
-    }
+    },
+    middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 })
 
 export default store
